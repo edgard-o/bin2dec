663f7f9a659f8b3cc0310d5515f467917ed9f941
edgard-o <edgard.ohnishi@gmail.com>
2021-10-04 14:55:05 -0300

    Added Implamentation and Style

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const [binaryText, setBinaryText] = useState('');
+    const [decimalText, setDecimalText] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (binaryText.match(/^[0-1]+$/g) === null) {
+            setErrorMessage('Enter either 0 or 1')
+
+            return
+        }
+
+        setErrorMessage('')
+
+        const reversedBinaryText = binaryText
+            .split('')
+            .map(Number)
+
+        const result = reversedBinaryText.reduce(
+            (accumulator, currentValue, idx) =>
+                accumulator + currentValue * Math.pow(2, idx)
+        )
+
+        setDecimalText(result)
+    };
+
+    return (
+        <>
+            <h1>Binary to Decimal Converter</h1>
+
+            <form onSubmit={onFormSubmit} class="col s6">
+                {errorMessage && <span style={{ color: 'red' }}> {errorMessage} </span>}
+                <br />
+                <div class="input-field col s6">
+                    <input
+                        className="validate"
+                        id="binary"
+                        autoComplete="off"
+                        type="text" name="binary"
+                        value={binaryText}
+                        onChange={e => setBinaryText(e.target.value)}
+                        maxLength="8"
+                    />
+                    <label htmlFor="binary">
+                        Binary Input
+                    </label>
+                </div>
+
+                <br />
+                <input type="submit" value="Submit" />
+                <br />
+                <label>
+                    Decimal output
+                    <input type="text" name="decimal" value={decimalText} disabled />
+                </label>
+            </form>
+        </>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
